e299c35ac12f1f836921c3b6fe64abc344bedf7f
NatBernal <nataliabernal.comoflo@gmail.com>
2025-09-17 17:37:23 -0500

    First test endpoints, still not implemented

---
+++ server/db.js
@@ -0,0 +1,4 @@
+export const connectDB = async () => {
+  // TODO: conectar a la base de datos
+  console.log("Pendiente de implementar conexión a la base de datos MongoDB");
+};
---
+++ server/models/parkingModel.js
@@ -0,0 +1,5 @@
+import { Schema, model } from "mongoose";
+
+const parkingSchema = new Schema({});
+
+export default model("Parking", parkingSchema);
