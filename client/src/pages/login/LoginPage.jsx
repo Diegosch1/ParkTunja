25982d679050cf4dc628d4384efb2aea9e86a416
Steven-Leon-007 <estivlinyor@gmail.com>
2025-09-17 23:20:39 -0500

    Implemented landing page and login screen

--- client/src/pages/login/LoginPage.jsx
+++ client/src/pages/login/LoginPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import InputComponent from "../../components/input/InputComponent";
+import ButtonComponent from "../../components/button/ButtonComponent";
+import logo from "../../assets/parktunja_logo.png";
+import logoNegative from "../../assets/parktunja_logo_negative.png";
+import "./LoginPage.css";
+import { useNavigate } from "react-router";
+
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
 
-export default LoginPage
+  const navigate = useNavigate();
+
+  return (
+    <div className="login-page">
+      <picture className="logo-login" onClick={() => navigate("/")}>
+        <source srcSet={logo} media="(max-width: 768px)" />
+        <img
+          src={logoNegative}
+          alt="Parktunja logo"
+        />
+      </picture>
+      <div className="login-image"></div>
+      <div className="login-form">
+        <h2>Iniciar sesión</h2>
+        <form>
+          <InputComponent
+            label="Correo electrónico"
+            name="email"
+            type="email"
+            placeholder="Ingresa tu correo"
+          />
+          <InputComponent
+            label="Contraseña"
+            name="password"
+            type="password"
+            placeholder="Ingresa tu contraseña"
+          />
+          <ButtonComponent text="Ingresar" type="primary" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
