88b5147663b9bc3b4122d4890f68eb47ba39471d
Diegosch1 <diegoale.rodriguez15@gmail.com>
2025-09-29 11:00:22 -0500

    added cors policy

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
+import morgan from 'morgan';
 import authRouter from "./routes/auth.routers.js";
 import parkingRouter from "./routes/parking.routers.js";
 import userRouter from "./routes/user.routers.js";
@@ -7,7 +8,12 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
